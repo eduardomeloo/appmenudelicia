92e061c6bf8459dbddd9081353f23f5de3ad8184
Eduardo <eduardo.peixoto.melo@hotmail.com>
2021-02-13 11:44:42 -0300

    deploy

---
+++ menudelicia/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+    <v-app>
+        <Header />
+        <v-main>
+            <HelloWorld />
+        </v-main>
+        <Footer />
+    </v-app>
+</template>
+
+<script>
+import Header from "@/components/templates/Header";
+import Footer from "@/components/templates/Footer";
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+    name: "App",
+
+    components: {
+        HelloWorld,
+        Header,
+        Footer
+    },
+
+    data: () => {
+       return {
+
+       } 
+    },
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ menudelicia/src/components/templates/Footer.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-footer padless>
+        <v-card class="flex" flat tile color="black">
+            <v-card-text class="py-3 white--text text-center">
+                Desenvolvido por <strong>MFT</strong> — {{ new Date().getFullYear() }}
+            </v-card-text>
+        </v-card>
+    </v-footer>
+</template>
+<script>
+export default {};
+</script>
+<style>
+</style>
